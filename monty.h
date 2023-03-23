39888dd945e4db93a30c6ac7dfc28640ef2eca5e
keme-ebi <kelvin6bolou@gmail.com>
2023-03-23 23:18:30 +0000

    added function prototypes inside header file

---
+++ monty.h
@@ -0,0 +1,12 @@
+#ifndef MONTY_H
+#define MONTY_H
+
+void f_add(stack_t **head, unsigned int counter);
+void f_nop(stack_t **head, unsigned int counter);
+void pall(stack_t **head, unsigned int counter);
+void f_pint(stack_t **head, unsigned int counter);
+void f_pop(stack_t **head, unsigned int counter);
+void f_sub(stack_t **head, unsigned int counter);
+void f_swap(stack_t **head, unsigned int counter);
+
+#endif
